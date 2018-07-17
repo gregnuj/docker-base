b0bf138aae8a40cbe230e976ec19b4f678440d18
Junge, Gregory (gj8287) <gj8287@us.att.com>
2018-07-17 00:29:56 +0000

    renamed slink.sh to supervisor_link.sh

--- rootfs/etc/entrypoint.d/slink.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# links supervisord files to debian location
-
-if [ -d "/etc/supervisor/conf.d" ]; then
-	for file in $(ls /etc/supervisor.d/*.ini); do
-		ln -s $file /etc/supervisor/conf.d/$(basename $file '.ini').conf
-	done
-fi
