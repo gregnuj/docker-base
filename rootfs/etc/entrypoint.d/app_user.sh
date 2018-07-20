30ed6351e784d70e1696139a0e807a34ff8de183
Junge, Gregory (gj8287) <gj8287@us.att.com>
2018-07-20 15:53:10 +0000

    fixed app_user.sh

--- rootfs/etc/entrypoint.d/app_user.sh
+++ rootfs/etc/entrypoint.d/app_user.sh
@@ -10,8 +10,8 @@ export APP_SSH="${APP_SSH:-${APP_HOME}/.ssh}"
 export APP_KEY="${APP_KEY:-${APP_SSH}/id_rsa}"
 export APP_AUTH="${APP_AUTH:-${APP_SSH}/authorized_keys}"
 
-addgroup -g ${APP_GID} ${APP_USER}
-adduser -D -u ${APP_UID} -G ${APP_USER} ${APP_USER}
+groupadd -g ${APP_GID} ${APP_USER}
+useradd -u ${APP_UID} -g ${APP_GID} ${APP_USER}
 
 echo "${APP_SUDO} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 echo "exec sudo -u "${APP_USER}" bash -l" >> /root/.profile
