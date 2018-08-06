acc71498756b5630d71263a397c5b90ee12b3cce
Junge, Gregory (gj8287) <gj8287@us.att.com>
2018-08-06 21:17:16 +0000

    modified app_user.sh for high numbered uids

--- rootfs/etc/entrypoint.d/app_user.sh
+++ rootfs/etc/entrypoint.d/app_user.sh
@@ -10,8 +10,17 @@ export APP_SSH="${APP_SSH:-${APP_HOME}/.ssh}"
 export APP_KEY="${APP_KEY:-${APP_SSH}/id_rsa}"
 export APP_AUTH="${APP_AUTH:-${APP_SSH}/authorized_keys}"
 
-addgroup -g ${APP_GID} ${APP_USER}
-adduser -D -u ${APP_UID} -G ${APP_USER} ${APP_USER}
+
+if [ "${APP_UID}" -lt 256000 ]; then
+    addgroup -g ${APP_GID} ${APP_USER}
+    adduser -D -u ${APP_UID} -G ${APP_USER} ${APP_USER}
+else 
+    # Create user https://stackoverflow.com/questions/41807026/cant-add-a-user-with-a-high-uid-in-docker-alpine
+    echo "${APP_USER}:x:${APP_UID}:${APP_GID}::/home/user:" >> /etc/passwd
+    echo "${APP_USER}:!:$(($(date +%s) / 60 / 60 / 24)):0:99999:7:::" >> /etc/shadow
+    echo "${APP_GROUP}:x:${APP_GID}:" >> /etc/group
+    cp /etc/skel ${APP_HOME} && chown -R ${APP_USER}:${APP_GROUP} ${APP_HOME}
+fi
 
 echo "${APP_SUDO} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 echo "exec sudo -u "${APP_USER}" bash -l" >> /root/.profile
