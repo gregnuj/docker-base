7a083592418a04f4058cc4cb79794b0aab097cd0
Junge, Gregory (gj8287) <gj8287@us.att.com>
2018-08-21 18:45:47 +0000

    moving all -setup scripts to usr/local/bin

--- rootfs/usr/local/bin/supervisor-setup.sh
+++ rootfs/usr/local/bin/supervisor-setup.sh
@@ -3,9 +3,9 @@
 # globals
 export APP_USER="${APP_USER:-cyclops}"
 export SUPERVISOR_INI=${SUPERVISOR_INI:-/etc/supervisor.d/default.ini}
+export SUPERVISOR_SECRET="${SUPERVISOR_SECRET:-/var/run/secrets/app_password}"
 
 # links supervisord files to debian location
-
 if [ -d "/etc/supervisor/conf.d" ]; then
 	for file in $(ls /etc/supervisor.d/*.ini); do
 		ln -s $file /etc/supervisor/conf.d/$(basename $file '.ini').conf
@@ -13,13 +13,12 @@ if [ -d "/etc/supervisor/conf.d" ]; then
 fi
 
 # Get supervisor password
-SUPERVISOR_SECRET="${SUPERVISOR_SECRET:-/var/run/secrets/app_password}"
 if [ -z "${APP_PASSWD}" ]; then
 	# Create webconsole secret if it does not exist
 	if [ ! -f "${SUPERVISOR_SECRET}" ]; then
 		openssl rand -base64 10 > ${SUPERVISOR_SECRET}
 	fi
-	APP_PASSWD="$(echo -n $(cat ${SUPERVISOR_SECRET}))"
+	APP_PASSWD="$(echo -n $(sudo cat ${SUPERVISOR_SECRET}))"
 fi
 
 
