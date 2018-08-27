56d45bd9053940bca965184dc80cbe329a0eb45f
Junge, Gregory (gj8287) <gj8287@us.att.com>
2018-08-27 16:34:45 +0000

    undoing changes for running as upriv

--- rootfs/etc/entrypoint.d/serial/app_user.sh
+++ rootfs/etc/entrypoint.d/serial/app_user.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-export APP_USER="${APP_USER:-cyclops}"
-export APP_GROUP="${APP_GROUP:-${APP_USER}}"
-export APP_SUDO="${APP_SUDO:-${APP_USER}}"
 export APP_UID="${APP_UID:-10000}"
 export APP_GID="${APP_GID:-${APP_UID}}"
+export APP_USER="${APP_USER:-cyclops}"
 export APP_HOME="${APP_HOME:-/home/${APP_USER}}"
+export APP_GROUP="${APP_GROUP:-${APP_USER}}"
+export APP_SUDO="${APP_SUDO:-${APP_USER}}"
 export APP_SSH="${APP_SSH:-${APP_HOME}/.ssh}"
 export APP_KEY="${APP_KEY:-${APP_SSH}/id_rsa}"
 export APP_AUTH="${APP_AUTH:-${APP_SSH}/authorized_keys}"
+export APP_SECRET="${APP_SECRET:-/var/run/secrets/app_password}"
 
 TAG="$(basename $0 '.sh')"
 
@@ -27,7 +28,8 @@ fi
 # Get/change passwd (for sudo)
 if [ -z "${APP_PASSWD}" ]; then
     # Create password if it does not exist
-    if [ -f "${APP_SECRET}" ]; then
+    if [ ! -f "${APP_SECRET}" ]; then
+	mkdir -p "$(dirname ${APP_SECRET})"
         openssl rand -base64 10 > ${APP_SECRET}
     fi
     APP_PASSWD="$(echo -n $(cat ${APP_SECRET}))"
@@ -39,6 +41,7 @@ echo "${APP_USER}:${APP_PASSWD}" | chpasswd
 # update sudoers
 echo "${TAG}: Adding sudo for ${APP_SUDO}"
 echo "${APP_SUDO} ALL=(ALL) ALL" >> /etc/sudoers
+
 
 echo "${TAG}: Adding ssh key in ${APP_SSH}"
 mkdir -p ${APP_SSH}
@@ -54,3 +57,6 @@ fi
 # needed for setup.ini
 echo "${TAG}: Setting ownership for ${APP_HOME}"
 chown -R ${APP_USER}:${APP_USER} ${APP_HOME}
+
+# use APP_HOME as HOME
+export HOME="${APP_HOME}"
--- rootfs/etc/entrypoint.d/serial/configs_copy.sh
+++ rootfs/etc/entrypoint.d/serial/configs_copy.sh
@@ -11,18 +11,18 @@ mkdir -p ${CONFIG_DIR}
 cd ${CONFIG_DIR}
 
 for dir in $(ls); do
-	for file in $(find $dir -type f); do
-		echo "${TAG} Copying ${CONFIG_DIR}/${file} to /${file}"
-		dirname="$(dirname ${file})"
-		mode="$(stat -c '%a' ${CONFIG_DIR}/${file})"
-		if [ ! -d ${dirname} ]; then
-			mkdir -m ${mode} -p ${dirname}
-		fi
-		if grep -q 'SUBOFF'  <<<"$(head -n1 ${file})"; then
-			cat < "${CONFIG_DIR}/${file}" > "/${file}"
-		else
-			envsubst < "${CONFIG_DIR}/${file}" > "/${file}"
-		fi
-		chmod $mode "/${file}"
-	done
+    for file in $(find $dir -type f); do
+        echo "${TAG} Copying ${CONFIG_DIR}/${file} to /${file}"
+        dirname="/$(dirname ${file})"
+        mode="$(stat -c '%a' ${CONFIG_DIR}/${file})"
+        if [ ! -d ${dirname} ]; then
+            mkdir -m ${mode} -p ${dirname}
+        fi
+        if grep -q 'SUBOFF'  <<<"$(head -n1 ${file})"; then
+            cat < "${CONFIG_DIR}/${file}" > "/${file}"
+        else
+            envsubst < "${CONFIG_DIR}/${file}" > "/${file}"
+        fi
+        chmod $mode "/${file}"
+    done
 done
