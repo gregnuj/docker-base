ab120cef54afe62592fc3eb78458cdfac87b0596
Greg Junge <gregnuj@gmail.com>
2018-11-24 00:08:34 +0000

    created

--- rootfs/usr/local/bin/super-tee.sh
+++ rootfs/usr/local/bin/super-tee.sh
@@ -4,10 +4,10 @@ declare -a PIDS
 
 for file in /var/log/supervisord/*.log; do 
     tail -F -n +1 $file &
-    PIDS="$PIDS $!"
+    PIDS+="$!"
 done
 
-for pid in $PIDS; do 
+for pid in ${PIDS[@]}; do 
 	wait $pid
 done
 
